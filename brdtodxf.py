5b83e61bd02620c3c02c3cf2f0ddddd9d89e5820
RomanBabchenko <46852404+RomanBabchenko@users.noreply.github.com>
2019-04-07 17:30:42 +0300

    Добавлен класс для работы с шифрованными файлами

--- brdtodxf.py
+++ brdtodxf.py
@@ -3,27 +3,80 @@ from dxfwrite import DXFEngine as dxf
 from colorama import init, Fore, Back
 import sys
 
-class Brdfile(object):
+class Brdfile_1(object):
     def __init__(self, path):
+        found = False
         with open(path, "r") as read_file:
+            self.type = 'brd1'
             self.lines = read_file.readlines()
             self.parts = {}
             self.pins = []
             for i in range(len(self.lines)):
                 if self.lines[i][:5] == 'PARTS':
+                    found = True
                     for n in range(i + 1, len(self.lines)):
                         if self.lines[n] != '\n':
                             part = self.lines[n][:-1].split()
                             self.parts[part[0]] = part[1:7]
                         else:
                             break
-
                 if self.lines[i][:4] == 'PINS':
                     for n in range(i + 1, len(self.lines)):
                         if self.lines[n] != '\n':
                             self.pins.append(self.lines[n][:-1])
                         else:
                             break
+            if not found:
+                raise BufferError
+
+class Brdfile_2(object):
+    def __init__(self, path):
+        with open(path, "rb") as f:
+            buffer = list(f.read())
+            if buffer[0:4] != [35, 226, 99, 40]:
+                raise BufferError
+        chars = ''
+        for i in range(len(buffer)):
+            if not (chr(buffer[i]) == '\r' or chr(buffer[i]) == '\n' or chr(buffer[i]) == 0):
+                buffer[i] = (~(buffer[i] >> 6 & 3 | buffer[i] << 2)) % 256
+            chars += chr(buffer[i])
+        self.type = 'brd2'
+        self.lines = chars.split('\r\n')
+        self.parts = {}
+        self.pins = []
+        found = False
+        for i in range(len(self.lines)):
+            if self.lines[i][:5] == 'Parts':
+                found = True
+                for n in range(i + 1, len(self.lines)):
+                    if self.lines[n] != '':
+                        part = self.lines[n].split()
+                        self.parts[part[0]] = part[1:7]
+                        try:
+                            self.parts[part[0]].append(int(self.lines[n - 1].split()[-1]))
+                        except ValueError:
+                            self.parts[part[0]].append('0')
+                    else:
+                        break
+
+            if self.lines[i][:4] == 'Pins':
+                for n in range(i + 1, len(self.lines)):
+                    if self.lines[n] != '':
+                        self.pins.append(self.lines[n])
+                    else:
+                        break
+        if not found:
+            raise BufferError
+
+    def range_xy(self, part):
+        x = []
+        y = []
+        for n in range(int(self.parts[part][2]), int(self.parts[part][1])):
+            side = self.pins[n].split()[3]
+            x.append(int(self.pins[n].split()[0]))
+            y.append(int(self.pins[n].split()[1]))
+        return [min(x), min(y), max(x), max(y), side]
+
 
 
 class Part(object):
@@ -83,10 +136,18 @@ def main():
     global part, width, diameter
     try:
         if sys.argv[1][-3:] != 'brd':
-            print('Неподдерживаемый формат файла.')
+            print('Неподдерживаемое разрешение файла.')
             return
         else:
-            file = Brdfile(sys.argv[1])
+            try:
+                file = Brdfile_1(sys.argv[1])
+            except BufferError:
+                try:
+                    file = Brdfile_2(sys.argv[1])
+                except BufferError:
+                    print('Неподдерживаемый формат файла')
+                    return
+
     except FileNotFoundError:
         print('file.brd не найден в папке с программой.')
         return
@@ -94,12 +155,16 @@ def main():
         print('Файл не указан.')
         return
 
+
     print(Back.BLUE + Fore.WHITE + ' BRDTODXF BY SERVICE CORE :) ' + Fore.RESET + Back.RESET)
     while True:
         try:
             print(Fore.RESET + 'Выберите позиционный номер объекта: ' + Fore.LIGHTRED_EX, end='')
             target = input()
-            part = Part(target, file.parts[target], file.pins)
+            if file.type == 'brd1':
+                part = Part(target, file.parts[target], file.pins)
+            if file.type == 'brd2':
+                part = Part(target, file.range_xy(target), file.pins)
             break
         except KeyError:
             print('Нет такого элемента')
@@ -136,9 +201,6 @@ def main():
     offset = (width - distance) / 2
     part.build_dxf(scale, offset, diameter)
 
-    # print(Fore.RED + 'some red text')
-    # print(Back.GREEN + 'and with a green background')
-    # print(Style.DIM + 'and in dim text')
 
 init(convert=True)
 main()
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import time
+
 with open("A1286_k18.brd", "rb") as f:
     buffer = bytearray(f.read())
 
@@ -9,6 +11,78 @@ for x in buffer:
     else:
         newbuf.append(x)
 
-
 with open('var.brd', mode='wb') as f:
     f.write(bytes(newbuf))
+
+
+def func2():
+    start = time.perf_counter()
+    with open("A1286_k18.brd", "rb") as f:
+        buffer2 = list(f.read())
+
+    chars = ''
+    for i in range(len(buffer2)):
+        if not (chr(buffer2[i]) == '\r' or chr(buffer2[i]) == '\n' or chr(buffer2[i]) == 0):
+            buffer2[i] = (~(buffer2[i] >> 6 & 3 | buffer2[i] << 2)) % 256
+        chars += chr(buffer2[i])
+    lines = chars.split('\r\n')
+    # print(lines)
+    with open('var2.brd', mode='wb') as f:
+        f.write(bytes(newbuf))
+
+    with open('var2.brd', "r") as read_file:
+        lines = read_file.readlines()
+    # print(lines)
+
+
+class Brdfile_2(object):
+    def __init__(self, path):
+        with open(path, "rb") as f:
+            buffer = list(f.read())
+        chars = ''
+        for i in range(len(buffer)):
+            if not (chr(buffer[i]) == '\r' or chr(buffer[i]) == '\n' or chr(buffer[i]) == 0):
+                buffer[i] = (~(buffer[i] >> 6 & 3 | buffer[i] << 2)) % 256
+            chars += chr(buffer[i])
+        self.lines = chars.split('\r\n')
+        self.parts = {}
+        self.pins = []
+        for i in range(len(self.lines)):
+            if self.lines[i][:5] == 'Parts':
+                print('Найден')
+                for n in range(i + 1, len(self.lines)):
+                    if self.lines[n] != '':
+                        part = self.lines[n].split()
+                        self.parts[part[0]] = part[1:7]
+                        try:
+                            self.parts[part[0]].append(int(self.lines[n - 1].split()[-1]))
+                        except ValueError:
+                            self.parts[part[0]].append('0')
+                    else:
+                        break
+
+            if self.lines[i][:4] == 'Pins':
+                for n in range(i + 1, len(self.lines)):
+                    if self.lines[n] != '':
+                        self.pins.append(self.lines[n])
+                    else:
+                        break
+
+    def range_xy(self, part):
+        x = []
+        y = []
+        print(range(int(self.parts[part][2]), int(self.parts[part][1])))
+        print(self.parts[part][1])
+        print(self.pins[int(self.parts[part][2])])
+        print(self.pins[int(self.parts[part][1])-1])
+        for n in range(int(self.parts[part][2]), int(self.parts[part][1])):
+            side = self.pins[n].split()[3]
+            x.append(int(self.pins[n].split()[0]))
+            y.append(int(self.pins[n].split()[1]))
+        return [min(x), min(y), max(x), max(y), side]
+
+print
+file = Brdfile_2('A1286_k18.brd')
+print(file.range_xy('U1000'))
+# print(file.parts['U8000'])
+print(func2())
